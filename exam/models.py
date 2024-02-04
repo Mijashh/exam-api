777f642b57c581047ccaf5ef90e862ee99451bb4
Mijash Adhikari <amijash@gmail.com>
2024-02-04 14:49:37 +0545

    feat: add required models

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class ExamIndex(models.Model):
+    name = models.CharField(max_length=200)
+    
+    university = models.CharField(max_length=100)
+    date=models.DateField()
+    slug=models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+
+class ExamDetails(models.Model):
+    index=models.ForeignKey(ExamIndex,on_delete=models.CASCADE)
+    description=models.TextField()
+    syallabus=models.TextField()
+    eligibility=models.TextField()
+    official_website=models.URLField()
+    courses_offered=models.TextField()
+    # model_papers=models.FileField(upload_to="static/Exam_Details/Model_Papers")
+    def __str__(self):
+        return self.index.name
