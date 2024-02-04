348f1a93499c073deecda85d8cef53dc858de966
Mijash Adhikari <amijash@gmail.com>
2024-02-04 14:50:34 +0545

    feat: add URL patterns for exam views

---
+++ exam/url.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import ExamDetailsView, ExamIndexView
+
+router = DefaultRouter()
+router.register('exam-index', ExamIndexView)
+router.register('exam-details', ExamDetailsView)
+
+urlpatterns=[
+    path('              ', include(router.urls))
+]
