1f4e5edf5296fcb6be15a20564990e83699c6054
Mijash Adhikari <amijash@gmail.com>
2024-02-04 14:51:00 +0545

    feat: create required viewsets

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import ExamDetails, ExamIndex
+from .serializers import ExamDetailsSerializer, ExamIndexSerializer
+
+
+class ExamIndexView(viewsets.ModelViewSet):
+    queryset = ExamIndex.objects.all()
+    serializer_class = ExamIndexSerializer
+    
+class ExamDetailsView(viewsets.ModelViewSet):
+    queryset = ExamDetails.objects.all()
+    serializer_class = ExamDetailsSerializer
