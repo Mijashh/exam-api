77adddb951751d57944866f0054b7e4b16bf485e
Mijash Adhikari <amijash@gmail.com>
2024-02-06 12:20:36 +0545

    feat: add autopopulate slug field if no slug mentioned in api call

--- exam/serializers.py
+++ exam/serializers.py
@@ -1,14 +1,22 @@
+from django.utils.text import slugify
 from rest_framework import serializers
 
-from .models import ExamDetails, ExamIndex
+from .models import ExamDetail, ExamIndex
 
 
 class ExamIndexSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExamIndex
         fields = '__all__'
+    
+    def create(self, validated_data):
+        validated_data['slug'] = slugify(validated_data['name'])
+        instance = ExamIndex.objects.create(**validated_data)
+        return instance
+
         
 class ExamDetailsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ExamDetails
+        model = ExamDetail
         fields = '__all__'
+
