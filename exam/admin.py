1211acbd682f3612ccdaf492fcd1423a7fc68298
Mijash Adhikari <amijash@gmail.com>
2024-02-06 12:19:46 +0545

    feat: add admin panel and register models

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ExamDetail, ExamIndex
+
+
+class ExamDetailsInline(admin.StackedInline):  
+    model = ExamDetail
+    extra = 1  
+    max_num=1
+
+
+class ExamIndexAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name','slug', 'university','date']}),
+    ]
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+    list_filter = ('university',"date")
+    inlines = [ExamDetailsInline]
+
+
+
+
+admin.site.site_header = "Exam Details Admin"
+admin.site.site_title = "Exam Details Admin Portal"
+admin.site.index_title = "Welcome to Exam Details Portal"
+admin.site.register(ExamIndex, ExamIndexAdmin)
