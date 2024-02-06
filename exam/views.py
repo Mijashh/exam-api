7a17771417a20a0cfbc106d898ad24b18ce3612f
Mijash Adhikari <amijash@gmail.com>
2024-02-06 12:21:05 +0545

    feat: add lookup field as slug to navigate

--- exam/views.py
+++ exam/views.py
@@ -1,14 +1,15 @@
 from rest_framework import viewsets
 
-from .models import ExamDetails, ExamIndex
+from .models import ExamDetail, ExamIndex
 from .serializers import ExamDetailsSerializer, ExamIndexSerializer
 
 
 class ExamIndexView(viewsets.ModelViewSet):
     queryset = ExamIndex.objects.all()
     serializer_class = ExamIndexSerializer
-    
+    lookup_field = 'slug'
     
 class ExamDetailsView(viewsets.ModelViewSet):
-    queryset = ExamDetails.objects.all()
+    queryset = ExamDetail.objects.all()
     serializer_class = ExamDetailsSerializer
+    lookup_field = 'index__slug'
