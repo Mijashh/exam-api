0a85e72b431f55cd63810daf4c3c7478a3cdc4c4
Mijash Adhikari <amijash@gmail.com>
2024-02-07 14:48:40 +0545

    refactor: format codes using ruff

--- exam/url.py
+++ exam/url.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ExamDetailsView, ExamIndexView
 
+from .views import ExamDetailsView, ExamIndexView
 
 router = DefaultRouter()
 router.register("exam-index", ExamIndexView)
