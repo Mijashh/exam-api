87aafa55d63a656aa1538e24efe36d0e5ab40b0e
Mijash Adhikari <amijash@gmail.com>
2024-02-07 12:55:45 +0545

    feat: add templates for form

--- exam_api/settings.py
+++ exam_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,20 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "exam",
     "rest_framework",
+    "django_filters",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+}
+TEMPLATES = [
+    {
+        "DIRS": [
+            os.path.join(
+                BASE_DIR, "templates"
+            ),  # Add your custom template directory here
+        ],
+    },
 ]
 
 
