e2cbe8e129b965e874a38a564b4242b112acc724
Mijash Adhikari <amijash@gmail.com>
2024-02-08 11:27:24 +0545

    feat: add token authentication url endpoint

--- exam/url.py
+++ exam/url.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from .views import ExamDetailsView, ExamIndexView
@@ -9,4 +10,5 @@ router.register("exam-detail", ExamDetailsView)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("token-auth/", views.obtain_auth_token),
 ]
